d9936e0adfa7752e00f70cdb59e80e2ddd083b7a
Seito Taka <amanoese@gmail.com>
2019-01-02 02:31:37 +0900

    add sample

---
+++ ruby/3d_acts_as_csv_class.rb
@@ -0,0 +1,27 @@
+class ActsAsCsv
+  def read
+    file = File.new(self.class.to_s.downcase + '.txt')
+    @headers = file.gets.chomp.split(',')
+    file.each { |row| @result << row.chomp.split(',') }
+  end
+
+  def headers
+    @headers
+  end
+
+  def csv_content
+    @result
+  end
+
+  def initialize
+    @result = []
+    read
+  end
+end
+
+class RubyCsv < ActsAsCsv
+end
+
+m = RubyCsv.new
+puts m.headers.inspect
+puts m.csv_content.inspect
