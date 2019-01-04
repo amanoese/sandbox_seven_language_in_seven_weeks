e732a92d8a8a507c2a7257f8a3212a4879816cc0
Seito Taka <amanoese@gmail.com>
2019-01-05 01:21:09 +0900

    finish the Prolog task of first day

---
+++ Prolog/1d_stady_favorite_books.pl
@@ -0,0 +1,10 @@
+#!swipl -f
+book('キノの旅','時雨沢恵一').
+book('学園キノ','時雨沢恵一').
+book('GGO','時雨沢恵一').
+book('BLAME!','弐瓶勉').
+book('NOiSE','弐瓶勉').
+book('BIOMEGA','弐瓶勉').
+book('ABARA','弐瓶勉').
+book('シドニアの騎士','弐瓶勉').
+book('人形の国','弐瓶勉').
---
+++ Prolog/1d_stady_musician_list.pl
@@ -0,0 +1,19 @@
+musician('椎野 きらり','ボーカル').
+musician('樫原 紗理奈','ギター').
+musician('前島 鹿子','ベース').
+musician('石動 千絵','ドラム').
+musician('吉本 結衣','ボーカル').
+musician('早川 麻美', 'ギター').
+musician('前島 祐子', 'ベース').
+musician('高嶋さん', 'キーボード').
+musician('西園寺さん', 'ドラム').
+
+genre('椎野 きらり',rock).
+genre('樫原 紗理奈',rock).
+genre('前島 鹿子',rock).
+genre('石動 千絵',rock).
+genre('吉本 結衣',rock).
+genre('早川 麻美',rock).
+genre('前島 祐子',rock).
+genre('高嶋さん',rock).
+genre('西園寺さん',rock).
