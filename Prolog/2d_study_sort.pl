4257fb7d6e10f4888fcde1d8a1a9b2403f137466
Seito Taka <amanoese@gmail.com>
2019-01-06 02:39:04 +0900

    Implement Simple Quick Sort

--- Prolog/2d_study_sort.pl
+++ Prolog/2d_study_sort.pl
@@ -1,12 +1,13 @@
 
-high_low_split(_,[],[],[]).
-high_low_split(T,[T|Tail],Lows,[T|Highs]) :- high_low_split(T,Tail,Lows,Highs).
-high_low_split(T,[Head|Tail],[Head|Lows],Highs) :- T > Head, high_low_split(T,Tail,Lows,Highs).
-high_low_split(T,[Head|Tail],Lows,[Head|Highs]) :- T < Head, high_low_split(T,Tail,Lows,Highs).
+high_low_split([T],[],[T],[]).
+high_low_split([T,T|Tail],Lows,[T|Sames],Highs) :- high_low_split([T|Tail],Lows,Sames,Highs).
+high_low_split([T,Head|Tail],[Head|Lows],Sames,Highs) :- T > Head, high_low_split([T|Tail],Lows,Sames,Highs).
+high_low_split([T,Head|Tail],Lows,Sames,[Head|Highs]) :- T < Head, high_low_split([T|Tail],Lows,Sames,Highs).
 
 q_sort([],[]).
-q_sort(Sorted,[Pibot|Tail]) :-
-  high_low_split(Pibot,Tail,Lows,Highs),
+q_sort(Sorted,List) :-
+  high_low_split(List,Lows,Sames,Highs),
   q_sort(LowSorted,Lows),
   q_sort(HighSorted,Highs),
-  append(LowSorted,[Pibot|HighSorted],Sorted).
+  append(LowSorted,Sames,LowAndSames),
+  append(LowAndSames,HighSorted,Sorted).
