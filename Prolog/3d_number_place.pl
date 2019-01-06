7e4622535aa4426b4ba67ec7cf394c4a528b585a
Seito Taka <amanoese@gmail.com>
2019-01-06 17:42:41 +0900

    add sudoku sample

---
+++ Prolog/3d_number_place.pl
@@ -0,0 +1,48 @@
+
+%sudoku([ _, _, 2, 3,
+%         _, _, _, _,
+%         _, _, _, _,
+%         3, 4, _, _, ]
+%       , Solution).
+
+main :-
+  sudoku([ _, _, 2, 3, _, _, _, _, _, _, _, _, 3, 4, _, _ ] , Solution),
+  print(Solution).
+
+sudoku(Puzzle,Solution) :-
+  use_module(library(clpfd)),
+  Solution = Puzzle,
+  Puzzle =
+  [ S11, S12, S13, S14
+  , S21, S22, S23, S24
+  , S31, S32, S33, S34
+  , S41, S42, S43, S44
+  ],
+  Puzzle ins 1..4,
+
+  Row1 = [ S11, S12, S13, S14 ],
+  Row2 = [ S21, S22, S23, S24 ],
+  Row3 = [ S31, S32, S33, S34 ],
+  Row4 = [ S41, S42, S43, S44 ],
+
+  Col1 = [ S11, S21, S31, S41],
+  Col2 = [ S12, S22, S32, S42],
+  Col3 = [ S13, S23, S33, S43],
+  Col4 = [ S14, S24, S34, S44],
+
+  Squaer1 = [ S11, S12, S21, S22],
+  Squaer2 = [ S13, S14, S23, S24],
+  Squaer3 = [ S31, S32, S41, S42],
+  Squaer4 = [ S33, S34, S43, S44],
+
+  valid(
+  [ Row1, Row2, Row3, Row4
+  , Col1, Col2, Col3, Col4
+  , Squaer1, Squaer2, Squaer3, Squaer4
+  ]).
+
+valid([]).
+valid([Head|Tail]) :-
+  all_different(Head),
+  valid(Tail).
+
